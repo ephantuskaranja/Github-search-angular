a1185f186c2b64d57dd5b4c9c077de2ddd92fb9c
Ephantus karanja <ephantusslogan@gmail.com>
2018-06-01 21:55:46 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { UsernameComponent } from './components/username/username.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsernameComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
